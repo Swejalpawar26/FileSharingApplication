832e3879069ffbb2f4ee2381b4d22e91cf933785
Swejalpawar26 <139461763+Swejalpawar26@users.noreply.github.com>
2024-04-05 22:15:20 +0530

    m2

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ root.iconphoto(False,image_icon)
 Label(root , text="File Tranfer" ,font=('Arial',20 ,'bold'),bg="#f4fdfe").place(x=20 ,y=30)
 Frame(root,width=400,height=2,bg ="#f3f5f6").place(x=25 ,y=80)
 
-sendImage = PhotoImage (file = "Images/sendS.png")
-send = Button(root , height= 50, width= 50, image=sendImage ,bg="#f4fdfe",bd = 0)
-send.place(x=70 ,y=120)
+sendImage = PhotoImage (file = "Images/send.png")
+send = Button(root ,image=sendImage ,bg="#f4fdfe",bd = 0)
+send.place(x=60 ,y=100)
 
 receiveImage = PhotoImage (file = "Images/receive.png")
-receive = Button(root , height= 50, width= 50, image = receiveImage ,bg="#f4fdfe",bd = 0)
-receive.place(x=300 ,y=120)
+receive = Button(root , image = receiveImage ,bg="#f4fdfe",bd = 0)
+receive.place(x=300 ,y=100)
 
 #label 
-Label(root,text ="Send", font=('acumin Variable Concept' , 17 ,'bold'),bg="#f4fdfe").place(x = 65 , y = 200)
-Label(root,text ="Receive", font=('acumin Variable Concept' , 17 ,'bold'),bg="#f4fdfe").place(x =280 , y = 200)
+Label(root,text ="Send", font=('acumin Variable Concept' , 17 ,'bold'),bg="#f4fdfe").place(x = 70 , y = 200)
+Label(root,text ="Receive", font=('acumin Variable Concept' , 17 ,'bold'),bg="#f4fdfe").place(x =300 , y = 200)
 
-background = PhotoImage(file ="Images/img1.png")
+background = PhotoImage(file ="Images/background.png")
 Label(root ,image = background).place(x=-2 , y =323)
 
 root.mainloop()
